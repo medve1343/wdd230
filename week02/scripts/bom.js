28af26cace2586ab61049fedcf986528ab5cf82c
medve1343 <medve1343@gmail.com>
2023-10-30 13:27:15 -0600

    week 2 done

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,29 +4,29 @@ const list = document.querySelector('#list');
 
 button.addEventListener('click', () => { 
     if (input.value != '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
+        li.textContent = input.value;
+        
+        deleteButton.textContent = '❌';
+        
+        li.append(deleteButton);
+        
+        list.append(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
     }
     else {
         return input.focus();
     }
  });
 
-const li = document.createElement('li');
 
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-
-deleteButton.textContent = '❌';
-
-li.append(deleteButton);
-
-list.append(li);
-
-deleteButton.addEventListener('click', () => {
-    list.removeChild(li);
-    input.focus();
-});
 
 input.focus();
 

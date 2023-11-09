ac97be927c0956af22021b8feb4737d4e70d9ac0
medve1343 <medve1343@gmail.com>
2023-11-09 15:48:45 -0700

    push_learning_activities

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -13,22 +13,31 @@ async function getProphetData() {
 
 function displayProphets(prophets) {
     prophets.forEach((prophet) => {
+        console.log(prophet);
         // Create elements to add to the div.cards element
     let card = document.createElement('section');
-    let fullName = document.createElement('__'); // fill in the blank
+    let fullName = document.createElement('h2'); // fill in the blank
     let portrait = document.createElement('img');
+    let birthday = document.createElement('p');
+    let birthplace = document.createElement('p');
 
     // Build the h2 content out to show the prophet's full name
-    fullName.textContent = `${prophet._____} ____________`; // fill in the blank
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
     // Build the image portrait by setting all the relevant attributes
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '340');
     portrait.setAttribute('height', '440');
+    
+    console.log(prophet.birthdate);
+    birthday.textContent = `${prophet.birthdate}`;
+    birthplace.textContent = `${prophet.birthplace}`;
 
     // Append the section(card) with the created elements
-    card.appendChild(_______); //fill in the blank
+    card.appendChild(fullName); //fill in the blank
+    card.appendChild(birthday);
+    card.appendChild(birthplace);
     card.appendChild(portrait);
 
     cards.appendChild(card);
--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/_______';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.77257&lon=6.65196&appid=e2a0ea60aba434d86831049a22c1e575';
 
 
 async function apiFetch() {
@@ -11,7 +11,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data); //testing
-            //displayResults(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -23,10 +23,12 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data._____}&deg;C`;
-    const iconsrc = `https://openweathermap.org/img/w/${______}.____`;
-    let desc = data.weather[0].____;
-    weatherIcon.setAttribute('____', ____);
-    weatherIcon.setAttribute('____', ____);
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    // console.log(currentTemp);
+    console.log(data.weather[0].icon);
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = `${desc}`;
 }

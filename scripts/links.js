a0bc9784f16515559dc21f60b3a7b58564485b8d
medve1343 <medve1343@gmail.com>
2023-11-14 09:14:25 -0700

    display

--- scripts/links.js
+++ scripts/links.js
@@ -5,12 +5,18 @@ const linksURL = "https://medve1343.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.weeks);
   }
 
   function displayLinks(data) {
-    data.forEach(week => {
-        console.log(week);
+    data.forEach((week) => {
+        let list = document.createElement('li');
+        let link = document.createElement('a');
+        
+
+        link.setAttribute('href', data.baseURL);
+        link.textContent = week.title;
+        list.appendChild(link);
     });
   }
   

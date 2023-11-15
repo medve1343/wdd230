1cc1b7c895011e302d1d13cc62e49b7619ee6040
medve1343 <medve1343@gmail.com>
2023-11-14 17:53:28 -0700

    push

--- scripts/links.js
+++ scripts/links.js
@@ -1,7 +1,7 @@
 const baseURL = "https://medve1343.github.io/wdd230/";
 
 const linksURL = "https://medve1343.github.io/wdd230/data/links.json";
-const links = document.querySelector('#links');
+let links = document.querySelector('#links');
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
@@ -10,17 +10,30 @@ async function getLinks() {
 
   function displayLinks(data) {
     console.log(data);
+    let index = 1;
     data.forEach((week) => {
         let list = document.createElement('li');
-        let link = document.createElement('a');
         
-        
-        link.setAttribute('href', data.baseURL);
-        link.textContent = week.title;
 
-        list.appendChild(link);
+        list.textContent = 'Week ' + index + ' ';
+        week.links.forEach((a) => {
+          let temp = document.createElement('a');
+          temp.setAttribute('href', a.baseURL);
+          temp.textContent = a.title + '      ';
+          
+          list.appendChild(temp);
+        });
+
+        // let link = document.createElement('a');
+        
+
+        // link.setAttribute('href', data.baseURL);
+        // link.textContent = week.title;
+
+        // list.appendChild(link);
 
         links.appendChild(list);
+        index++;
     });
   }
   
